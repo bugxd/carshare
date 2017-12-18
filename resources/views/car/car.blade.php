7e47de900396e5e740596f93d434cc66d5413774
bugxd <l.feistritzer@gmx.at>
2017-12-18 10:41:48 +0100

    add image to cardata
    TODO change name for navbar-example

--- resources/views/car/car.blade.php
+++ resources/views/car/car.blade.php
@@ -16,6 +16,13 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
+                <div class="card">
+                    <img class="card-img-top" src="img/car1.jpg" alt="Card image car1">
+                    <div class="card-body">
+                        <h4 class="card-title">Card title</h4>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
                 <div id="navbar-example">
                     <div id="list-example" class="list-group">
                         <a class="list-group-item list-group-item-action" href="#list-item-1">Car Data</a>
