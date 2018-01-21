338ebdab76c2eea53d3589890094bfe70399c03e
Danii19 <Stefanics93@gmail.com>
2018-01-21 20:58:27 +0100

    AGB

--- resources/views/infos/agb.blade.php
+++ resources/views/infos/agb.blade.php
@@ -1,7 +1,19 @@
-<?php
-/**
- * Created by IntelliJ IDEA.
- * User: sebit
- * Date: 27.12.2017
- * Time: 20:54
- */
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <p>
+    <br><h5>Verbraucher - AGB</h5><br>
+    <h6>Allgemeine Gesch&aumlftsbedingungen f&uumlr die Nutzung der &uumlber die Website carshare zug&aumlnglichen Services :</h6><br>
+    <h6>1. Geltungsbereich</h6>
+    Diese Allgemeinen Gesch&aumlftsbedingungen gelten f&uumlr s&aumlmtliche zwischen Carshare und dem privaten Nutzer der Carshare Services im Rahmen der Nutzung der Fahrzeug-Datenbank entstehenden Rechtsverh&aumlltnisse.<br><br>
+    <h6>2. Verantwortlichkeit f&uumlr Inhalte</h6>
+    Der Nutzer ist f&uumlr die von ihm eingestellten Inhalte selbst verantwortlich. Er steht insbesondere daf&uumlr ein, dass die Inhalte nicht rechtswidrig sind und keine Rechte Dritter (z.B. Urheber- oder Markenrechte) verletzen. Carshare ist nicht zur Pr&uumlfung verpflichtet, ob ein eingestellter Inhalt Rechte Dritter beeintr&aumlchtigt oder gegen gesetzliche Vorschriften verst&oumlsst. Der Nutzer ist verpflichtet, die von ihm eingestellten Inhalte zu berichtigen bzw. zu l&oumlschen, sofern sie nicht den Anforderung an die Gestaltung von Anzeigen entsprechen.<br><br>
+     <h6>3. Nutzungsrechte</h6>
+     Durch das &Uumlbermitteln von Inseraten r&aumlumt der Nutzer Carshare an den &uumlbermittelten Inhalten die folgenden nichtausschliesslichen, &uumlbertragbaren, zeitlich und r&aumlumlich unbeschr&aumlnkten Rechte (einschliesslich des Rechts zur Erteilung von Unterlizenzen) ein:
+	das Archivierungs- und Datenbankrecht, d.h. das Recht, die Inhalte in jeder Form zu archivieren und insbesondere auch digitalisiert zu erfassen, in Datenbanken einzustellen und auf allen bekannten Speichermedien und auf beliebigen Datentr&aumlgern zu speichern und mit anderen Werken oder Werkteilen zu verbinden;
+	das Vervielf&aumlltigungs- und Verbreitungsrecht, d.h. das Recht, die Inhalte beliebig zu speichern, zu vervielf&aumlltigen und in elektronischen oder anderen Medien (z. B. Internet, Zeitungen, Zeitschriften) ganz oder teilweise zug&aumlnglich zu machen oder zu verbreiten. <br><br>
+    <h6>4. Haftung</h6>
+    F&uumlr Sch&aumlden, die dem Nutzer im Zusammenhang mit den Services von Carshare entsteht, gilt Folgendes: Bei Vorsatz oder grober Fahrl&aumlssigkeit sowie bei Vorliegen einer Garantie ist die Haftung unbeschr&aumlnkt. Bei leichter Fahrl&aumlssigkeit ist die Haftung im Falle der Verletzung von Leben, K&oumlrper oder Gesundheit ebenfalls unbeschr&aumlnkt. Bei einer leicht fahrl&aumlssigen Verletzung wesentlicher Vertragspflichten ist die Haftung beschr&aumlnkt auf darauf zur&uumlckzuf&uumlhrende Sach- und Verm&oumlgenssch&aumlden in H&oumlhe des vorhersehbaren, typischerweise eintretenden Schadens. Jede weitergehende Haftung auf Schadensersatz ist, ausser f&uumlr Anspr&uumlche nach dem Produkthaftungsgesetz, ausgeschlossen.
+</div>
+@endsection
